00c08166a48cb008dbcbd279e2b36e18074bfa20
SekineNezerova <7b4gz6k@code.edu.az>
2022-03-31 13:34:18 -0700

    commited homework

---
+++ 3103.2022HomeTask/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3103._2022HomeTask
+{
+    static class Calculator
+    { public static int Sum(int a,int b)
+        {
+          
+            return a+b;
+        }
+        public static int Subtract(int a, int b)
+        {
+
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ 3103.2022HomeTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3103._2022HomeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+         
+         
+           
+            
+        }
+    }
+}
